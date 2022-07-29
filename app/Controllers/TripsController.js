4ab3465a69633d58e895f218012dfa40c7e29dfc
kade fullmer <fullmer24@gmail.com>
2022-07-29 14:59:44 -0600

    break

---
+++ app/Controllers/TripsController.js
@@ -0,0 +1,25 @@
+// import { ProxyState } from "../AppState.js"
+
+
+
+
+
+
+// function _drawTrip() {
+//     let template = ''
+//     let trips = ProxyState.trips.sort((a, b) => a.date - b.date)
+//     console.log(template);
+//     trips.forEach(t => template += t.Template)
+//     document.getElementById('trip').innerHTML = template
+// }
+// _drawTrip()
+
+
+// export class TripsController {
+//     constructor() {
+//         console.log(`trip contoller working`);
+//         ProxyState.on('trips', _drawTrip)
+//         ProxyState.on('reservations', _drawTrip)
+
+//     }
+// }
--- app/main.js
+++ app/main.js
@@ -1,7 +1,9 @@
+import { TripsController } from "./Controllers/TripsController.js";
 import { ValuesController } from "./Controllers/ValuesController.js";
 
 class App {
   // valuesController = new ValuesController();
+  tripsController = new TripsController();
 }
 
 window["app"] = new App();
